17c83181e74c4da662a7650f738fadababbd9d8c
Sachan <asachan@r3aig.com>
2018-04-15 15:20:14 +0100

    Updated

--- Exercise6.py
+++ Exercise6.py
@@ -1,10 +1,10 @@
 #adarsha sachan
 #Exercise 6 factorial function
-
+x=int(input("Please enter the value"))
 def facrorial(x):
   i=1
   for j in range (i,x+1):
     i=i*j
   return (i)
-print (facrorial(5))
+print (facrorial(x))
 
