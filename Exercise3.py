ff9578ab70a6d021ece65fcfcaea12e619302551
Sachan <asachan@r3aig.com>
2018-04-16 15:02:34 +0100

    updated

--- Exercise3.py
+++ Exercise3.py
@@ -4,7 +4,7 @@
 i=int(input("Please enter the value"))
 while i!=1:
   #print(i)
-  # if divide by 2 if even, multiply by three and 1 if odd  using a while loop 
+  # if divide by 2 if even, multiply by three and 1 if odd  using a while loop
   if (i % 2)==0:
       i = int(i / 2)
       #print(i)
